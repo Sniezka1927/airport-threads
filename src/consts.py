ab556c3a2edf9689dfe7840c1ee354314b0cfdf0
Sniezka <dygons14@gmail.com>
2025-01-17 20:34:06 +0100

    Implement luggage control process

--- src/consts.py
+++ src/consts.py
@@ -13,13 +13,24 @@ PASSENGER_GENERATION_MAX_DELAY = 1  # seconds
 
 # Sćieżki do plików
 ENTRANCE_FILE = "../data/entrance.json"
+LUGGAGE_CHECKED_FILE = "../data/luggage_checked.json"
+LUGGAGE_REJECTED_FILE = "../data/luggage_rejected.json"
 LOGS_FILE = f"../logs/logs_{floor(time())}.txt"
 LOGS_DIRECTORY = "../logs"
+
+
+AIRPORT_LUGGAGE_LIMIT = 12.5  # kg
 
 # Wiadmości wysyłane przez procesy
 class MESSAGES:
     # Generator
     NEW_PASSENGER = 'Wygenerowano pasażera'
+    # Luggage
+    LUGGAGE_CHECK_BEGIN = 'Sprawdzam pasażera'
+    LUGGAGE_CHECK_OK = 'przeszedł kontrolę bagażową'
+    LUGGAGE_CHECK_REJECT = "odrzucony - za ciężki bagaż"
 # Nazwy lokalizacji w procesach
 class LOCATIONS:
     ENTRANCE = 'ENTRANCE'
+    LUGGAGE = 'LUGGAGE'
+    
