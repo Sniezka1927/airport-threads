eb3f6966d8d4b4a86faf7f5cf651db6954846929
Sniezka <dygons14@gmail.com>
2025-01-18 20:47:33 +0100

    Update constants

--- src/consts.py
+++ src/consts.py
@@ -17,9 +17,15 @@ LUGGAGE_CHECKED_FILE = "../data/luggage_checked.json"
 LUGGAGE_REJECTED_FILE = "../data/luggage_rejected.json"
 LOGS_FILE = f"../logs/logs_{floor(time())}.txt"
 LOGS_DIRECTORY = "../logs"
-
+SECURITY_CHECKED_FILE = "../data/security_checked.json"
+SECURITY_REJECTED_FILE = "../data/security_rejected.json"
 
 AIRPORT_LUGGAGE_LIMIT = 12.5  # kg
+
+# Konfiguracja kontroli pasażerów
+SECURITY_CHECKPOINTS_COUNT = 3
+MAX_PASSENGERS_PER_CHECKPOINT = 2
+MAX_CONTROL_PASSES = 3 # max num of passes through security control
 
 # Wiadmości wysyłane przez procesy
 class MESSAGES:
@@ -29,8 +35,14 @@ class MESSAGES:
     LUGGAGE_CHECK_BEGIN = 'Sprawdzam pasażera'
     LUGGAGE_CHECK_OK = 'przeszedł kontrolę bagażową'
     LUGGAGE_CHECK_REJECT = "odrzucony - za ciężki bagaż"
+    # Security
+    SECURITY_CONTROL_BEGIN = "rozpoczyna kontrolę bezpieczeństwa"
+    SECURITY_CONTROL_OK = "przeszedł kontrolę bezpieczeństwa"
+    SECURITY_CONTROL_REJECT = "odrzucony - znaleziono niebezpieczne przedmioty"
+
 # Nazwy lokalizacji w procesach
 class LOCATIONS:
     ENTRANCE = 'ENTRANCE'
     LUGGAGE = 'LUGGAGE'
+    SECURITY = 'SECURITY'
     
